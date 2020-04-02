8fcdb039c007cad5800046933a9b1a189da61ad6
EdiJunior88 <er.moura88@yahoo.com.br>
2020-04-02 00:40:14 -0300

    Programação com JavaScript, além de HTML e CSS

---
+++ Aula03/aula03.js
@@ -0,0 +1,8 @@
+// Compartilhe código no Codepen
+
+suaidade = prompt("######### Qual a sua idade manolete? #########")
+document.write(suaidade, " anos e ")
+document.writeln(suaidade * 365, " dias")
+document.write("<br>", "********************", "<br>")
+//<br> é quebra de linha
+document.write("Cê tá meio veinho hein?")
